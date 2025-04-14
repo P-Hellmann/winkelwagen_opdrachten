f5a5f067b37511148b8184a1a210242215a93882
P-Hellmann <302053776@student.rocmondriaan.nl>
2025-04-14 11:30:14 +0200

    comment update

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Form\OrderType;
 use App\Storage\CartSessionStorage;
 use http\Header;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CartController extends AbstractController
 {
     #[Route('/cart', name: 'app_cart')]
-    public function index(CartSessionStorage $cartSessionStorage): Response
+    public function index(CartSessionStorage $cartSessionStorage, Request $request): Response
     {
         $shoppingCart = $cartSessionStorage->getShoppingCart();
+
+        $Order = new Order;
+        $form = $this->createForm(OrderType::class, $Order);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $order = $form->getData();
+            // purchases koppelen aan order
+        }
 
         return $this->render('cart/index.html.twig', [
             'shoppingCart' => $shoppingCart,
